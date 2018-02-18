1a92e286d3ee54977f0ff4666922b4478b55195b
Wietze <wietze@users.noreply.github.com>
2018-02-18 23:03:45 +0000

    Improved HTML indenting

--- js/twit.js
+++ js/twit.js
@@ -1,8 +1,6 @@
 
 $.getJSON( "https://www.helpdeskweb.nl/partners/wietze/tweet.php?ts="+ Math.round(+new Date()/1000), function( data ) {
-//<?PHP echo $tweet.' <a href="http://www.twitter.com/wietze/status/'.$tweets[0]['id'].'/" style="color: #aaa; font-size: 12px; margin-left: 10px;">'.timeAgo($tweets[0]['created_at']).'</a>'; ?>
-
-  contents = data['tweet'] + ' <a href="http://www.twitter.com/wietze/status/' + data['id'] + '/" style="color: #aaa; font-size: 12px; margin-left: 10px;">' + data['time'] + '</a>';
+  contents = data['tweet'] + ' <a href="https://www.twitter.com/wietze/status/' + data['id'] + '/" style="color: #aaa; font-size: 12px; margin-left: 10px;">' + data['time'] + '</a>';
   $('#twit').append(contents);
 });
 
