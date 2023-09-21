1be17f3022f7a979f17315a2badc9499f4bc3708
Wietze <wietze@users.noreply.github.com>
2023-09-21 18:47:42 +0100

    Updating main page

--- js/twit.js
+++
@@ -1,6 +0,0 @@
-
-$.getJSON( "https://www.helpdeskweb.nl/partners/wietze/tweet.php?www&ts="+ Math.round(+new Date()/1000), function( data ) {
-  contents = data['tweet'] + ' <a href="https://www.twitter.com/wietze/status/' + data['id'] + '" style="color: #aaa; font-size: 12px; margin-left: 10px;">' + data['time'] + '</a>';
-  $('#twit').append(contents);
-});
-
